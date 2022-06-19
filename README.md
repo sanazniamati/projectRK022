6c51817bbd9ec0d749a457a0f81946a4530482bb
sanaz-niamati <sanaz.niamati@yahoo.com>
2022-06-19 14:57:31 +0430

    first

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-![rect](./rect.PNG)
+# React Konva
+![this is an image](https://cloud.githubusercontent.com/assets/1443320/12193428/3bda2fcc-b623-11e5-8319-b1ccfc95eaec.png)
+
+React Konva is a JavaScript library for drawing complex canvas graphics using [React](https://reactjs.org/)
+
+It provides declarative and reactive bindings to the [Konva Framework](https://konvajs.org/).
+
+# picture Demo
+
+![Star](./img.png)
+
+**How To create a star with react- Konva ? **
--- src/index.js
+++ src/index.js
@@ -1,37 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Stage, Layer, Rect } from "react-konva";
+import { Stage, Layer, Star } from "react-konva";
 
 const App = () => {
   return (
     <Stage width={window.innerWidth} height={window.innerHeight}>
       <Layer>
-        <Rect
-          x={20}
-          y={20}
-          width={100}
-          height={50}
-          fill="green"
-          stroke="black"
-          strokWidth={4}
-        />
-        <Rect
+        <Star
           x={150}
-          y={40}
-          width={100}
-          height={50}
-          fill="red"
-          shadowBlur={10}
-          stroke="black"
-          cornerRadius={10}
-        />
-        <Rect
-          x={50}
-          y={120}
-          width={100}
-          height={100}
-          fill="blue"
-          cornerRadius={[0, 10, 20, 30]}
+          y={150}
+          numPoints={6}
+          innerRadius={40}
+          outerRadius={70}
+          fill={"yellow"}
+          stroke={"black"}
+          strokeWidth={4}
         />
       </Layer>
     </Stage>
